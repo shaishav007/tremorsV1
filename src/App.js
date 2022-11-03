322ee3fdc5a0096aa878fad4b7797849e20c83e2
shaishav007 <shaishav007@gmail.com>
2022-11-03 17:21:36 -0400

    initial API test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+//testing apis starts now.
+useEffect(()=>{
+
+  axios({
+    url:'https://earthquake.usgs.gov/fdsnws/event/1/query',
+    params:{
+      format:'geojson',
+      starttime:'2022-10-03',
+      endtime:'2022-11-03',
+      orderby:'magnitude',
+      latitude:43.651070, 
+      longitude:-79.347015,
+      maxradiuskm:1000,
+    }
+  }).then((res)=>{
+    console.log(res.data);
+
+  }).catch((err)=>{
+    console.log(err);
+  })
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
+//since we have 1000s in a day, we will see if limiting a region gives us something manageable
