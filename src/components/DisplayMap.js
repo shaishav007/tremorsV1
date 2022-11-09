05eed64c4d0eb4f68a1a8dad2a340779cb8bcbef
Tristan <tristanthorburn@gmail.com>
2022-11-09 17:46:51 -0500

    Custome filtered markers

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -1,5 +1,9 @@
 
 import { MapContainer, TileLayer, Popup,useMap ,Marker} from 'react-leaflet';
+import L from 'leaflet';
+import boomSvg from './assets/boomSvg.svg'
+
+
 
 //this is exactly like any other component just defined inside the file instead of outside, THIS IS responsible for changing the center
 function ChangeView({center,zoom}){
@@ -7,13 +11,31 @@ function ChangeView({center,zoom}){
     map.setView(center,zoom);
     return null;
 }
+// Icon options
+
+// Create custom icon
+const customIcon = (mag) => {
+    let addedClass = 'markerGeology'
+    if (mag > 3.5){
+        addedClass= 'markerRich'
+    }
+    else if (mag > 6){
+        addedClass= 'markerStrong'
+    } else if (mag > 7){
+        addedClass= 'markerAll'
+    }
+    return L.icon({
+        iconUrl:boomSvg,
+        iconSize: [75, 75],
+        className:addedClass
+    })
+}
 
 const DisplayMap = (props) => {
     // console.log(props.markerPopupInfo[0].coords[1],props.markerPopupInfo[0].coords[0])
     console.log('marker popup info',props.markerPopupInfo);
     return (
         <div>
-
                 {/* Top container is considered immutable so a changeView or any other named object has to be below this as a child */}
                 <MapContainer center={[props.latitude,props.longitude]} zoom={50} scrollWheelZoom={false}> 
                      {/* just made a component here and tried the setView function on this */}
@@ -28,7 +50,10 @@ const DisplayMap = (props) => {
                             props.markerPopupInfo!==[]?
                             props.markerPopupInfo.map((item)=>{
                                 return(
-                                <Marker position={[item.coords[1],item.coords[0]]} className="marker">
+                                <Marker 
+                                    position={[item.coords[1],item.coords[0]]} 
+                                    icon={customIcon(item.popupInfo.magnitude)}
+                                    >
                                     <Popup>
                                     Magnitude:{item.popupInfo.magnitude}, <br/>
                                     Place:{item.popupInfo.place},
