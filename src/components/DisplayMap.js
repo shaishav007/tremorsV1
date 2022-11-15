6a3889e22abd092634b013886f2152dfd76d08ad
shaishav007 <shaishav007@gmail.com>
2022-11-15 15:00:25 -0500

    3d test begins

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -41,41 +41,7 @@ const DisplayMap = (props) => {
 
     return (
         <div className='mapComponentsContainer'>
-                <Legend earthquakesFound={props.markerPopupInfo.length}/>
-                {/* Top container is considered immutable so a changeView or any other named object has to be below this as a child */}
-                <MapContainer center={[props.latitude,props.longitude]} zoom={2} scrollWheelZoom={false}> 
-                     {/* just made a component here and tried the setView function on this */}
-                    <ChangeView center={[props.latitude,props.longitude]} zoom={2} scrollWheelZoom={false} />
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        {/* The data is stored in markerPopupInfo, for each item in it generate a marker component. */}
-                      
-                      
-                        {
-                            
-                            props.markerPopupInfo!==[]?
-                            props.markerPopupInfo.map((item)=>{
-                                return(
-                                <Marker 
-                                    position={[item.coords[1],item.coords[0]]} 
-                                    icon={customIcon(item.popupInfo.magnitude)}
-                                    key={item.id}
-                                    
-                                    >
-                                    <Popup>
-                                    Magnitude:{item.popupInfo.magnitude}, <br/>
-                                    Place:{item.popupInfo.place},
-                                    responder:{item.popupInfo.responder}
-                                    
-                                    </Popup>
-                                </Marker>
-                                )
-                            })
-                            :<></>
-                        }
-                </MapContainer>
+                
         </div>
   )
 }
